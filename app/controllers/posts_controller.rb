18b450388ce8b75beb43d0a472babe12ec1b2c77
Bomin-Lee <pairagon0@gmail.com>
2017-01-23 01:54:44 +0900

    Make CRUD
    finish

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,38 @@
 class PostsController < ApplicationController
   def index
+    @post_list = Post.all
   end
 
   def show
+    @detail_post = Post.find(params[:post_id])
   end
 
   def new
   end
 
   def create
+    new_list = Post.new
+    new_list.title = params[:title]
+    new_list.content = params[:content]
+    new_list.save
+    redirect_to "/posts/index"
   end
 
   def edit
+    @edit_post = Post.find(params[:post_id])
   end
 
   def update
+    edited_post = Post.find(params[:post_id])
+    edited_post.title = params[:title]
+    edited_post.content = params[:content]
+    edited_post.save
+    redirect_to "/posts/index"
   end
 
   def delete
+    delete_post = Post.find(params[:post_id])
+    delete_post.destroy
+    redirect_to "/posts/index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root 'posts#index'
   get 'posts/index'
-  get 'posts/show'
+  get 'posts/show/:post_id' => 'posts#show'
   get 'posts/new'
   post 'posts/create'
-  get 'posts/edit'
-  post 'posts/update'
-  post 'posts/delete'
+  get 'posts/edit/:post_id' => 'posts#edit'
+  post 'posts/update/:post_id' => 'posts#update'
+  get 'posts/delete/:post_id' => 'posts#delete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
