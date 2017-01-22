c5090351a79fa701752e0e4a48bbe576f1c6d4cc
Bomin-Lee <pairagon0@gmail.com>
2017-01-23 00:38:08 +0900

    initial commit2
    Set model, view, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  post 'posts/create'
+  get 'posts/edit'
+  post 'posts/update'
+  post 'posts/delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
